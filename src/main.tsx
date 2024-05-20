71a86cfbefa92fa261892e96a0bcd7d83c02bf4f
jhondharkyson <jhondharkyson@gmail.com>
2024-05-20 19:44:58 -0300

    Adicionando onload - refatorar links Header

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import Home from './pages/index.tsx'
 import Header from './components/Header/index.tsx'
 import Footer from './components/Footer/index.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Header/>
-    <Home />
-    <Footer/>
-  </React.StrictMode>,
-)
+window.onload = function() {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Header />
+      <Home />
+      <Footer />
+    </React.StrictMode>
+  );
+};
